ad23ad1e6bae372ea504e6ff4756652bf696a2e7
gregd72002@gmail.com <gregd72002@gmail.com@a9bfaba8-ec6e-0adc-4b23-1352bed8afe3>
2011-10-13 19:33:06 +0000

    R2

--- Blue GPS/src/mypackage/MyScreen.java
+++ Blue GPS/src/mypackage/MyScreen.java
@@ -1,16 +1,8 @@
 package mypackage;
 
-import java.util.Timer;
-
-import javax.microedition.location.Location;
-
-import net.rim.device.api.command.Command;
-import net.rim.device.api.system.Application;
 import net.rim.device.api.ui.MenuItem;
-import net.rim.device.api.ui.UiApplication;
 import net.rim.device.api.ui.container.MainScreen;
 import net.rim.device.api.ui.decor.BackgroundFactory;
-import net.rim.device.api.util.StringProvider;
 
 /**
  * A class extending the MainScreen class, which provides default standard
@@ -22,9 +14,10 @@ public final class MyScreen extends MainScreen
      * Creates a new MyScreen object
      */
 	public LogField logf;
-	private Location loc; 
-	private long lastGPSRead;
-	public MenuItem MenuStart;
+	private MenuItem MenuStart;
+	private MenuItem MenuOptions;
+	private MenuItem MenuHelp;
+	private MenuItem MenuAbout;
 	
     public MyScreen()
     {        
@@ -32,20 +25,23 @@ public final class MyScreen extends MainScreen
         setTitle("Blue GPS");
         setBackground(BackgroundFactory.createSolidBackground(0));
         
-        MenuStart = new MenuItem(new StringProvider("Start"), 0x230000, 0);    
-        MenuStart.setCommandContext(this);
-        StartCommandHandler sch = new StartCommandHandler();
-        MenuStart.setCommand(new Command(sch));
+        MenuStart = new AppMenu("Start", 0x230000, 0,0,this);    
         addMenuItem(MenuStart);
 
+        MenuOptions = new AppMenu("Options", 0x230000, 1, 1, this);    
+        addMenuItem(MenuOptions);
+        
+        MenuHelp = new AppMenu("Help", 0x230000, 2,2, this);
+        addMenuItem(MenuHelp);
+        
+        MenuAbout = new AppMenu("About", 0x230000, 3,3, this);    
+        addMenuItem(MenuAbout);
+        
         this.logf = new LogField();
         
         this.add(logf);
         this.logf.setText("Press start...");
+     
     }
     
-    
-    public String getName() {
-    	return "MyScreen";
-    }
 }
